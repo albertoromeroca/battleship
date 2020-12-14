1da9997a972ce729146a2d9f8be9369e51c5fb88
Daniel Montesinos <danimontesinos1@gmail.com>
2020-12-14 10:25:11 +0100

    Automated Tests executes all the tests in the Tests folder
    - Deleted some redundant definitions.

--- src/test/java/BattleShip/AutomatedTests.java
+++ src/test/java/BattleShip/AutomatedTests.java
@@ -1,47 +1,58 @@
 package BattleShip;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class AutomatedTests {
 
 	public static void main(String[] args) throws IOException {
-		Reader myTest1 = new Reader("src/Tests/test1.txt");
+		String[] pathnames;
+		File f = new File("src/Tests");
+		pathnames = f.list();
 		
-		ArrayList<Square> squaresOne = new ArrayList<Square>();
-		ArrayList<Square> squaresTwo = new ArrayList<Square>();
+		int totalTests = pathnames.length;
+		int correctTest = 0;
 		
-		Board board1 = new Board(squaresOne);
-		Board board2 = new Board(squaresTwo);
-	
-		GameView view = new GameView();
-		ScannerInterface sc = new MockScanner();
+		for(int j=0; j<totalTests; j++) {
+			System.out.println("//////////////Starting Test "+pathnames[j]+"//////////////");
 		
-		PlayerInterface player1 = new MockPlayer(myTest1.getElement(), board1);
-		PlayerInterface player2 = new MockPlayer(myTest1.getElement(), board2);
-		
-		GameController game = new GameController(player1, player2, view, sc);
-		
-		ArrayList<Square> squares1 = new ArrayList<Square>();
-		ArrayList<Square> squares2 = new ArrayList<Square>();
-		
-		
-		for(int i=0; i<(myTest1.getNumElements()/2)-1; i++) {
-			game.getScanner().setChar(myTest1.getElement().charAt(0));
-			game.getScanner().setInt(Integer.parseInt(myTest1.getElement()));
+			Reader myTest1 = new Reader("src/Tests/"+pathnames[j]);
 			
-			if(i%2==0) {
-				game.turn(player1, player2);
-			} else {
-				game.turn(player2, player1);
+			ArrayList<Square> squaresOne = new ArrayList<Square>();
+			ArrayList<Square> squaresTwo = new ArrayList<Square>();
+			
+			Board board1 = new Board(squaresOne);
+			Board board2 = new Board(squaresTwo);
+		
+			GameView view = new GameView();
+			ScannerInterface sc = new MockScanner();
+			
+			PlayerInterface player1 = new MockPlayer(myTest1.getElement(), board1);
+			PlayerInterface player2 = new MockPlayer(myTest1.getElement(), board2);
+			
+			GameController game = new GameController(player1, player2, view, sc);
+			
+			
+			for(int i=0; i<(myTest1.getNumElements()/2)-1; i++) {
+				game.getScanner().setChar(myTest1.getElement().charAt(0));
+				game.getScanner().setInt(Integer.parseInt(myTest1.getElement()));
+				
+				if(i%2==0) {
+					game.turn(player1, player2);
+				} else {
+					game.turn(player2, player1);
+				}
+				
 			}
 			
+			System.out.println("//////////////Test '"+pathnames[j]+"' passed//////////////\n");
+			correctTest++;
 		}
 		
-		//System.out.println(game.getWin());
+		System.out.println("/////////////////////////////////////////////////////////");
+		System.out.println("//////////////[Total successful tests: "+correctTest+"/"+totalTests+"]//////////////");
+		System.out.println("/////////////////////////////////////////////////////////");
 	}
 
 }
